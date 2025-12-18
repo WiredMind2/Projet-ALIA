a6d689f08d614adb062a2584e1b1fdd7d75d839c
GonzalesAndy <andygonzales83@gmail.com>
2025-12-18 11:26:43 +0100

    Rename minimax configurations for consistency in naming convention

--- tournament_config.pl
+++ tournament_config.pl
@@ -10,11 +10,11 @@ ai_config(random1, random, []).
 ai_config(presque_random1, presque_random, []).
 
 % Minimax WITHOUT Alpha-Beta Pruning
-ai_config(minimax_d2_noab, minimax, [depth(2), alphabeta(false)]).
-ai_config(minimax_d3_noab, minimax, [depth(3), alphabeta(false)]).
+ai_config(mm_d2_noab, minimax, [depth(2), alphabeta(false)]).
+ai_config(mm_d3_noab, minimax, [depth(3), alphabeta(false)]).
 
 % Minimax WITH Alpha-Beta Pruning
-ai_config(minimax_d3_ab, minimax, [depth(3), alphabeta(true)]).
-ai_config(minimax_d4_ab, minimax, [depth(4), alphabeta(true)]).
-ai_config(minimax_d5_ab, minimax, [depth(5), alphabeta(true)]).
+ai_config(mm_d3_ab, minimax, [depth(3), alphabeta(true)]).
+ai_config(mm_d4_ab, minimax, [depth(4), alphabeta(true)]).
+ai_config(mm_d5_ab, minimax, [depth(5), alphabeta(true)]).
 
